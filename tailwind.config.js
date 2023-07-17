88575d571b4103785c0dcb66a63b8cae6d051bba
Nur Muhammad Rizky <nrmrizky5@gmail.com>
2023-07-17 14:54:04 +0700

    make profile card componenet

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        darkCyan: "hsl(185, 75%, 39%)",
+        veryDarkDesatured: "hsl(229, 23%, 23%)",
+        darkGrayishBlue: "hsl(227, 10%, 46%)",
+        darkGray: "hsl(0, 0%, 59%)",
+      },
+    },
+  },
+  plugins: [],
+};
